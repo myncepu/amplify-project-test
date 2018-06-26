201c729adaf004da75557becc368916fe44ee843
Yan Pengqiang <yan2010@live.com>
2018-06-26 21:52:46 +0800

    Connect to Your Backend

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import aws_exports from './src/aws-exports';
+
+Amplify.configure(aws_exports)
 
 export default class App extends React.Component {
   render() {
